5c920177f93a800e5c069827bb4629086e2422a8
Julianpella78 <julianpella@hotmail.com>
2017-05-05 20:13:47 -0300

    inicio proyecto

---
+++ PruebaCapas/brlSistema/brlClientes.vb
@@ -0,0 +1,31 @@
+﻿Imports dtlSistema
+Public Class brlClientes
+
+    Public Sub obtenerRegistro(ByVal idcliente As Integer, ByRef clientes As DataTable)
+        Dim x As New dtlClientes
+        x.obtenerRegistro(idcliente, clientes)
+
+    End Sub
+
+    Public Sub insertarRegistro(ByVal idcliente As Integer, ByRef strrazonsocial As String)
+        Dim x As New dtlClientes
+        x.insertarRegistro(idcliente, strrazonsocial)
+    End Sub
+
+    Public Function ExisteCliente(ByVal idcliente As Integer) As Boolean
+        Dim x As New dtlClientes
+        Dim clientes As New DataTable
+        x.obtenerRegistro(idcliente, clientes)
+
+        If clientes.Rows.Count > 0 Then
+            ExisteCliente = True
+        Else
+            ExisteCliente = False
+        End If
+    End Function
+
+    Public Sub eliminarRegistro(ByVal idcliente As Integer)
+        Dim x As New dtlClientes
+        x.eliminarRegistro(idcliente)
+    End Sub
+End Class
---
+++ PruebaCapas/dtlSistema/dtlClientes.vb
@@ -0,0 +1,68 @@
+﻿Imports System.Data.SqlClient
+Public Class dtlClientes
+
+    Private Property oConn As SqlConnection
+
+
+
+    Public Sub obtenerRegistro(ByRef idcliente As Integer, ByRef clientes As DataTable)
+        oConn = New SqlConnection("Server=DIENAMOVIL\SQLEXPRESS;integrated security=true;database=Segpool")
+        If oConn.State = 1 Then oConn.Close()
+        oConn.Open()
+        Dim table As New DataTable
+        Dim Adp As New SqlDataAdapter()
+        Adp.SelectCommand = New SqlCommand() ' Creando una Instancia de SqlCommand
+        Adp.SelectCommand.Connection = oConn 'Conexión
+        Adp.SelectCommand.CommandText = "sp_obtenerRegistro"
+        Adp.SelectCommand.CommandType = CommandType.StoredProcedure
+        Adp.SelectCommand.Parameters.Add("@idcliente", SqlDbType.Int, 4)
+        Adp.SelectCommand.Parameters("@idcliente").Value = idcliente
+        Adp.Fill(table)
+        clientes = table
+
+
+    End Sub
+    Public Sub insertarRegistro(ByRef intidcliente As Integer, ByRef strrazonSocial As String)
+        oConn = New SqlConnection("Server=DIENAMOVIL\SQLEXPRESS;integrated security=true;database=Segpool")
+        If oConn.State = 1 Then oConn.Close()
+        oConn.Open()
+        Dim cmd As New SqlCommand
+        Dim param(1) As SqlParameter
+
+        param(0) = New SqlParameter("@idcliente", intidcliente)
+        param(1) = New SqlParameter("@razonsocial", strrazonSocial)
+
+        cmd.CommandType = CommandType.StoredProcedure
+        cmd.CommandText = "sp_insertarRegistro"
+        cmd.Connection = oConn
+        cmd.Parameters.AddRange(param)
+
+
+        cmd.ExecuteNonQuery()
+
+
+
+    End Sub
+
+    Public Sub eliminarRegistro(ByRef intidcliente As Integer)
+        oConn = New SqlConnection("Server=DIENAMOVIL\SQLEXPRESS;integrated security=true;database=Segpool")
+        If oConn.State = 1 Then oConn.Close()
+        oConn.Open()
+        Dim cmd As New SqlCommand
+        Dim param(0) As SqlParameter
+
+        param(0) = New SqlParameter("@idcliente", intidcliente)
+
+
+        cmd.CommandType = CommandType.StoredProcedure
+        cmd.CommandText = "sp_eliminarRegistro"
+        cmd.Connection = oConn
+        cmd.Parameters.AddRange(param)
+
+
+        cmd.ExecuteNonQuery()
+
+
+
+    End Sub
+End Class
